ba3702c8fcc364eef94f7b41664fe23e7b830f04
Vivek Kumar <vivek.raj7557@gmail.com>
2022-06-21 21:33:01 +0530

    CODE: Smart-contact-manager CHANGES:   Making user specific dashboard.

--- smart-contact-manager/src/main/java/com/smart/smartcontactmanager/controller/UserController.java
+++ smart-contact-manager/src/main/java/com/smart/smartcontactmanager/controller/UserController.java
@@ -1,14 +1,34 @@
 package com.smart.smartcontactmanager.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smart.smartcontactmanager.entity.User;
+import com.smart.smartcontactmanager.repository.UserRepository;
 
 @Controller()
 @RequestMapping("/user")
 public class UserController {
-	
+
+	@Autowired
+	UserRepository userRepository;
+
 	@RequestMapping("/dashboard")
-	public String dashboard() {
+	public String dashboard(Model model, Principal principal ) {
+		String username = principal.getName();
+		System.out.println("USERNAME :"+username);
+
+		try {
+			User u = userRepository.getUserByUserName(username);
+			model.addAttribute(u);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return "user/dashboard";
 	}
 
